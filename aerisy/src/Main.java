be914774826ffc959cd2c61c4931e7f14fd90a41
aerisy <124805662+aerisy43@users.noreply.github.com>
2023-02-26 19:30:41 +0300

    Update Main.java

--- aerisy/src/Main.java
+++ aerisy/src/Main.java
@@ -1,39 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         MySolution.main();
-
-        /*System.out.println("Задание 1");
+       
+        System.out.println("Задание №1");
         System.out.println("");
 
-        Phone phone1 = new Phone("89006665454", "WHATTAPHONk2", 451);
-        Phone phone2 = new Phone("82940684955", "IphoneProMax1923", 100);
-        Phone phone3 = new Phone("81679434576", "Chel203", 666);
+        Phone phone1 = new Phone("78106762835", "bluckberry", 420);
+        Phone phone2 = new Phone("78036482135", "iphone", 222);
+        Phone phone3 = new Phone("78976986712", "samsung", 648);
         System.out.println("");
 
-        phone1.receiveCall("Мила");
+        phone1.receiveCall("skyel");
         System.out.println(phone1.GetNumber());
-        phone2.receiveCall("Владочка");
+        phone2.receiveCall("k0n4");
         System.out.println(phone2.GetNumber());
-        phone3.receiveCall("Чел");
+        phone3.receiveCall("haatt");
         System.out.println(phone3.GetNumber());
         System.out.println("");
 
-        phone1.receiveCall("Мила", "82940684955");
-        phone2.receiveCall("Владочка", "82940684955");
-        phone3.receiveCall("Чел", "81679434576");
+        phone1.receiveCall("skyel", "78106762835");
+        phone2.receiveCall("k0n4", "78036482135");
+        phone3.receiveCall("haatt", "78976986712");
         System.out.println("");
 
-        String[] nums = new String[]{"82940684955", "82347435745", "81679434576"};
+        String[] nums = new String[]{"78106762835", "78036482135", "78976986712"};
         Phone.sendMessage(nums);
         System.out.println("");
         System.out.println("");
 
-        System.out.println("Задание 2");
+        System.out.println("Задание №2");
 
         System.out.println("");
         Person nn = new Person();
-        Person Mila = new Person("Прокопенко Владислава Геннадьевна", 17);
-        Mila.move(Mila.fullName);
-        Mila.talk(Mila.fullName);*/
+        Person Maxim = new Person("Максим Беседин Александрович", 17);
+        Maxim.move(Maxim.fullName);
+        Maxim.talk(Maxim.fullName);
     }
 }
